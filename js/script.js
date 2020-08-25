5ff9370db2c1a38e24f2256b857673feaf7583a4
Sally Young <github@justa.fish>
2020-08-25 09:52:45 +0100

    New key

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const youTubePlayer = require('yt-player');
  * Videos
  */
 // Generated at https://console.developers.google.com/apis/credentials
-const youTubeKey = 'AIzaSyD1DeOmS0xz-j6kQcPzxP4iGxJ9gaYQ_oE';
+const youTubeKey = 'AIzaSyBzFkg69RVtBRdgFDvn2xZ2xxjLARz3oaQ';
 
 const player = new youTubePlayer('#video-player');
 const list = document.querySelector('.video-thumbnails');
